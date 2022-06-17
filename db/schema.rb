30c100e18be707aca29fecb1971b945608afd1d2
satokofuwa <sato.fuwa@nifty.com>
2022-06-17 18:27:54 +0900

    usersテーブルにuid providerカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_17_073016) do
+ActiveRecord::Schema.define(version: 2022_06_17_092633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -154,6 +154,8 @@ ActiveRecord::Schema.define(version: 2022_06_17_073016) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["task_id"], name: "index_users_on_task_id"
