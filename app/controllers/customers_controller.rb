122540d40f99ebcafeae45bbda1affcc934fa7bd
satokofuwa <sato.fuwa@nifty.com>
2022-06-18 00:20:06 +0900

    顧客情報詳細画面　天気予報表示画面作成

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,19 @@
 class CustomersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_customer, only: [:show]
+ 
+  def show
+    @tasks=Task.all
+    @task=@tasks.find(@customer.task_id)
+   
+  end
+  private
+  def customer_params 
+    params.require(:customer).permit(:name,:tel,:zip,:prefecture,:address,:room_number)
+  end
+
+ def set_customer
+  #@customer=@task.customers.id
+  @customer = Customer.find(params[:id])
+ end
 end
