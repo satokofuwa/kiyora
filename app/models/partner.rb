af44918777e31be8eee651279f19ce15086d9e0b
satokofuwa <sato.fuwa@nifty.com>
2022-06-17 21:45:18 +0900

    partner.rb アソシエーション validation作成

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,2 +1,7 @@
 class Partner < ApplicationRecord
+  has_many :task_partners, dependent: :destroy
+  has_many :tasks, through: :task_partners, source: :task
+  with_options presence: true do
+    validates :name
+  end
 end
