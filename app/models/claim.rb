8987415cc27a9376d8329501109d37546be1ff35
satokofuwa <sato.fuwa@nifty.com>
2022-06-17 21:42:40 +0900

    claim.rb アソシエーション validation作成

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -1,2 +1,7 @@
 class Claim < ApplicationRecord
+  belongs_to :task
+  validates :troubled_at, presence: true
+  validates :category, presence: true
+  validates :content, presence: true
+  validates :support_content, presence: true
 end
