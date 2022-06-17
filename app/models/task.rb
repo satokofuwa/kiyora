03754dd1ab238b0a9038c61a37138747ea0bf8cc
satokofuwa <sato.fuwa@nifty.com>
2022-06-17 21:36:13 +0900

    task.rb validation追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,14 @@
 class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :dispatch, presence: true
+  validates :category, presence: true
+  validates :support_at, presence: true
+  validates :confirmation, presence: true
+  validates :step, presence: true
+  validates :vehicle, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :sales_check, presence: true
+  validates :admin_check, presence: true
+  validates :sales, presence: true
 end
