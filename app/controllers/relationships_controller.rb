1cedf10ad7fc9392e6158e9df1d6b7839443f093
satokofuwa <sato.fuwa@nifty.com>
2022-06-18 10:34:49 +0900

    relationship コントローラ作成

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,16 @@
 class RelationshipsController < ApplicationController
+  respond_to? :js
+  def create
+    if user_signed_in?
+      @user = User.find(params[:relationship][:followed_id])
+      current_user.follow!(@user)
+      flash[:notice] = "フォローしました"
+    end  
+  end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow!(@user)
+    flash[:notice] = "フォロー削除しました"
+  end
 end
