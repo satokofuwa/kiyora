2ad5c8db0904a9c510f8838c270916446112d3a7
satokofuwa <sato.fuwa@nifty.com>
2022-06-18 20:24:06 +0900

    404 500 エラー画面作成

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user && user.admin?
+    if user.try(:admin?)
       can :access, :rails_admin
       can :manage, :all
     end
