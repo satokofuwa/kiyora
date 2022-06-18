2b28bce9ecd8e05db5b5fb36cfe0b92195077a69
satokofuwa <sato.fuwa@nifty.com>
2022-06-18 10:58:08 +0900

    enum設定

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -4,4 +4,5 @@ class Claim < ApplicationRecord
   validates :category, presence: true
   validates :content, presence: true
   validates :support_content, presence: true
+  enum category: { "要対応": 1, "様子見": 2, "緊急対応":3, "申し送り":4}
 end
