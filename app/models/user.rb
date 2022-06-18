e9ea5f16b327e88ee432b1e68e47d7f0a379aad9
satokofuwa <sato.fuwa@nifty.com>
2022-06-18 10:47:39 +0900

    ajaxを使ったボタンの切り替え　jsファイル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tasks
@@ -8,6 +6,17 @@ class User < ApplicationRecord
   has_many :passive_relationships, foreign_key: 'followed_id', class_name:'Relationship', dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :following, through: :active_relationships, source: :followed
+  
+  def follow!(other_user)
+    active_relationships.create!(followed_id: other_user.id)
+  end
+  #フォローしているかどうかを確認する
+  def following?(other_user)
+    active_relationships.find_by(followed_id: other_user.id)
+  end
+  def unfollow!(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy   
+  end
 
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
