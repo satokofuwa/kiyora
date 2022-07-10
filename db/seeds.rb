63d497acd13535cd9a668af1f2b859578884984a
satokofuwa <sato.fuwa@nifty.com>
2022-07-11 00:52:57 +0900

    uid, provider 設定の為ゲストログインできない機能実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -179,17 +179,31 @@ end
 =======
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   
   5.times do |n| #管理者作成
 =======
 =======
+=======
+>>>>>>> 541afd4 (uid, provider 設定の為ゲストログインできない機能実装)
 
 =======
 >>>>>>> f5604c1 (uid provider設定によるゲストログインができない件を修正)
 >>>>>>> de3efff (uid provider設定によるゲストログインができない件を修正)
 >>>>>>> eb18b28 (uid provider設定によるゲストログインができない件を修正)
   5.times do |n| #ユーザー作成
+<<<<<<< HEAD
 
+=======
+<<<<<<< HEAD
+>>>>>>> f5604c1 (uid provider設定によるゲストログインができない件を修正)
+=======
+=======
+  
+  5.times do |n| #管理者作成
+>>>>>>> 83cfb1f (uid, provider 設定の為ゲストログインできない機能実装)
+>>>>>>> 541afd4 (uid, provider 設定の為ゲストログインできない機能実装)
+>>>>>>> 801ba37 (uid, provider 設定の為ゲストログインできない機能実装)
     name  = Gimei.kanji
     email = Faker::Internet.email
     tel = Faker::Number.number(digits: 11)
